b1aac89d0a8ffd211c1e092fd4e93a38cdde300a
Tommy Koning <99039195@mydavinci.nl>
2018-12-18 09:19:32 +0100

    compiling error removed

--- intro-tour-admin/src/app/service/admin-login.service.ts
+++ intro-tour-admin/src/app/service/admin-login.service.ts
@@ -35,7 +35,7 @@ export class AdminLoginService {
         console.log(locate);
         return this.http.post(this.apiUrl + locate, data, httpData).subscribe(
             res => {
-                localStorage.setItem('currentUser', JSON.stringify({token: res.success.token}));
+                localStorage.setItem('currentUser', JSON.stringify({token: res['success'].token}));
                 this.route.navigateByUrl('/');
             },
             err => {
